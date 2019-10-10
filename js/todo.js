acbbf01c4e23b33a0b27b895a94c5530ae32d267
Sivasankar98 <sivasankar@ideas2it.com>
2019-10-10 20:58:01 +0530

    Feature updated partially

--- js/todo.js
+++ js/todo.js
@@ -17,32 +17,76 @@ function important() {
         document.getElementById("important").style.color = '#0078d7';
     }
 }
+document.getElementById("open-side-bar").addEventListener("click", openNav);
 
 let LIST = [];
 let id;
 var numberOfList = 0;
-const input = document.getElementById("newlist");
-const list = document.getElementById("list");
-function addTodo(text, id){
-    const task = `<li>
-                    <p>${text} </p>
+function addTodo(list, id) {
+    const text = `<li>
+                    <p id=${id} onclick="getId(id)">
+                    <i class="icon fontIcon ms-Icon ms-Icon--BulletedList2 iconSize-24" aria-hidden="true"></i>
+                    ${list} </p>
                   </li>`;
     const position = "beforeend";
-    list.insertAdjacentHTML(position,task);
+    document.getElementById("list").insertAdjacentHTML(position, text, id);
 }
-input.addEventListener("keyup", function(event){
-    if(event.keyCode == 13){
-        const toDo = input.value;
+
+const input = document.getElementById("newlist");
+input.addEventListener("keyup", function(){
+    if(event.keyCode == 13) {
+        const todoList = input.value;
         id = ++numberOfList;
-        if(toDo){
-            addTodo(toDo, id);
+        status = true;
+        if(todoList) {
+            addTodo(todoList, id, status);
             LIST.push(
                 {
-                name: toDo,
-                id: id
+                    name: todoList,
+                    id: id,
+                    status: status
                 }
             );
         }
         input.value = "";
+        document.getElementById("listName").textContent = todoList;
     }
 });
+const listName = document.getElementById("list");
+listName.addEventListener("click", getId);
+
+function getId(listId) {
+    for(var i=0, iLen=LIST.length; i<iLen; i++) {
+        if(LIST[i].id == listId) {
+            document.getElementById("listName").textContent = LIST[i].name;
+        }
+    }
+}
+
+document.getElementById("listName").addEventListener("click", updateListName);
+
+function updateListName() {
+
+}
+
+document.getElementById("updateListName").addEventListener("click", toggleEditor);
+
+function toggleEditor() {
+   var theText = document.getElementById('updateListName');
+   var theEditor = document.getElementById('ta1');
+   var editorArea = document.getElementById('editor');
+   var subject = theText.innerHTML;
+   theEditor.value = subject;
+   theText.style.display = 'none';
+   editorArea.style.display = 'inline';
+}
+
+function doEdit() {
+    var theText = document.getElementById('updateListName');
+    var theEditor = document.getElementById('ta1');
+    var editorArea = document.getElementById('editor');
+    var subject = theEditor.value;
+    theText.innerHTML= subject;
+    theText.style.display = 'inline';
+    editorArea.style.display = 'none';
+ }
