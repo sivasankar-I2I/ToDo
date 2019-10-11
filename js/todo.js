0e0cc1c2b97c82b019a11af06726231a081eaa6b
Sivasankar98 <sivasankar@ideas2it.com>
2019-10-11 19:09:46 +0530

    Added subTasks

--- js/todo.js
+++ js/todo.js
@@ -1,8 +1,13 @@
+
+
+document.getElementById("open-side-bar").addEventListener("click", openNav);
+
 function openNav() {
-    if (document.getElementById("titleDiv").style.width=="240px") {
-        document.getElementById("titleDiv").style.width="0px";
+    if (document.getElementById("sidebar").style.width=="290px") {
+        document.getElementById("sidebar").style.width="48px";
+        document.getElementById("sidebar").style.width="48px";
     } else {
-        document.getElementById("titleDiv").style.width="240px";
+        document.getElementById("sidebar").style.width="290px";
     }
 }
 
@@ -17,59 +22,75 @@ function important() {
         document.getElementById("important").style.color = '#0078d7';
     }
 }
-document.getElementById("open-side-bar").addEventListener("click", openNav);
 
-let LIST = [];
-let id;
+var LIST = [];
+var id;
+var listInfo;
 var numberOfList = 0;
-function addTodo(list, id) {
+
+/**
+ *  Method used to display the list Name 
+ * @param {*} list 
+ * @param {*} id 
+ */
+function displayLists(list, id) {
     const text = `<li>
                     <p id=${id} onclick="getId(id)">
-                    <i class="icon fontIcon ms-Icon ms-Icon--BulletedList2 iconSize-24" aria-hidden="true"></i>
+                    <i class="ms-Icon ms-Icon--BulletedList2 iconSize-24 listcolor" aria-hidden="true" ></i>
                     ${list} </p>
                   </li>`;
     const position = "beforeend";
     document.getElementById("list").insertAdjacentHTML(position, text, id);
 }
 
+/**
+ * Method used to create the new list 
+ */
 const input = document.getElementById("newlist");
 input.addEventListener("keyup", function(){
     if(event.keyCode == 13) {
-        const todoList = input.value;
-        id = ++numberOfList;
-        status = true;
-        if(todoList) {
-            addTodo(todoList, id, status);
-            LIST.push(
-                {
-                    name: todoList,
-                    id: id,
-                    status: status
-                }
-            );
+        var listObject = {
+            name: "",
+            status: "",
+            id: "",
+            subtask: []
+        };
+        listObject.name = input.value;
+        listObject.id = ++numberOfList;
+        listObject.status = true;
+        if(listObject.name) {
+            displayLists(listObject.name, listObject.id);
+            LIST.push(listObject);
         }
         input.value = "";
-        document.getElementById("listName").textContent = todoList;
+        document.getElementById("listName").textContent = listObject.name;
+        listInfo = listObject;
     }
 });
-const listName = document.getElementById("list");
-listName.addEventListener("click", getId);
 
+/**
+ * Method used to get the list by click 
+ */
 function getId(listId) {
     for(var i=0, iLen=LIST.length; i<iLen; i++) {
         if(LIST[i].id == listId) {
             document.getElementById("listName").textContent = LIST[i].name;
-        }
+            getSublist(LIST[i].subtask);
+            listInfo = LIST[i];        }
     }
 }
 
-document.getElementById("listName").addEventListener("click", updateListName);
-
-function updateListName() {
-
+function getSublist(subtasks) {
+    let subTaskList = document.getElementById("subtasklist");
+    subTaskList.innerHTML = "";
+    for(let j=0; j<subtasks.length; j++) {
+        console.log(subtasks[j].name);
+        addsubtasks(subtasks[j].name);
+    }
 }
 
-document.getElementById("updateListName").addEventListener("click", toggleEditor);
+
+/*document.getElementById("updateListName").addEventListener("click", toggleEditor);
 
 function toggleEditor() {
    var theText = document.getElementById('updateListName');
@@ -89,4 +110,32 @@ function doEdit() {
     theText.innerHTML= subject;
     theText.style.display = 'inline';
     editorArea.style.display = 'none';
- }
+ }*/
+
+ /**
+  * Method to add subtasks
+  */
+ 
+const tasks = document.getElementById("tasks");
+let subId = 0;
+tasks.addEventListener("keyup", function(event) {
+    if(event.keyCode === 13) {
+        subtaskname = tasks.value;
+        addsubtasks(subtaskname);
+        listInfo.subtask.push({
+            name: subtaskname,
+            subTaskId: ++subId
+        })
+        tasks.value = "";
+    }
+});
+
+function addsubtasks(subtaskname) {
+    const text = `<li>
+                    <p id=${id}>
+                    ${subtaskname} </p>
+                  </li>`;
+    const position = "beforeend";
+    let subTaskList = document.getElementById("subtasklist");
+    subTaskList.insertAdjacentHTML(position, text);
+}
