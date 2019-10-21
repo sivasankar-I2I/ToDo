8d028a1290cb5c36d60fda0e6ca0e8bcb4ebd28f
Sivasankar98 <sivasankar@ideas2it.com>
2019-10-21 20:39:38 +0530

    X

--- js/todo.js
+++ js/todo.js
@@ -1,13 +1,22 @@
 
 let LIST = [];
+
 let listInfo;
+
 let taskInfo;
+
 let numberOfList = 0;
+
 let subId = 0;
+
 let newList = $("#newlist");
+
 let tasks = $("#new-tasks");
+
 let steps = $("#newStep");
+
 let list = $("#list");
+
 let stepId = 0;
 
 init();
@@ -25,9 +34,6 @@ function addEventListeners(element, action, method) {
     element.on(action, method);
 }
 
-function getElementById(id) {
-    return document.getElementById(id);
-}
 
 /**
  * Method used to toggle the sidebar 
@@ -36,6 +42,7 @@ function toggleNavbar() {
     $("#sidebar").toggleClass("sidebar toggle-width-open");
 }
 
+$("#listName").val("Tasks");
 /**
  *  Method used to display the list Name in the sidebar
  * @param {*} list 
@@ -96,13 +103,13 @@ function updateListName() {
  * Method used to get the list by using the listId
  */
 function getId(listId) {
-    listName(getSelectedList(listId));
+    listName(getListById(listId));
 }
 
 /**
  * Method used to return the list object in the particular id
  */
-function getSelectedList(id) {
+function getListById(id) {
     for(let i=0; i<LIST.length; i++) {
         if(LIST[i].id == id) {
             return LIST[i];
@@ -137,30 +144,34 @@ function displayTask(subTasks) {
  */
 function displaySubTasks(task) {
     let div = $(document.createElement("div"));
+    let p = $(document.createElement("p"));
     div.attr("class", "subtask-div");
     div.click(function(e){toggleStep()});
     let inputDiv = $(document.createElement("div"));
     inputDiv.attr("class", "task-checkbox");
-    let checkbox = $(document.createElement("input"));
-    checkbox.attr("type", "checkbox");
-    checkbox.attr("task", task);
-    checkbox.click(function(e){checkStatus(task)});
-    inputDiv.append(checkbox);
+    let image = $(document.createElement("img"));
+    if(task.status) {
+        image.attr("src", "images/circle-unchecked.svg");
+        p.attr("class", "taskText-div");
+    } else {
+        image.attr("src", "images/circle-checked.svg");
+        p.attr("class", "taskText-div-p");
+    }
+    image.attr("task", task);
+    image.click(function(e){checkStatus(task)});
+    inputDiv.append(image);
     div.append(inputDiv);
     let text = $(document.createElement("div"));
     text.attr("id", "taskNameList");
-    let p = $(document.createElement("p"));
-    if(task.status) {
-        p.attr("class", "taskText-div");
-    } else {
-        p.attr("class", "taskText-div-p");
-    }
     p.attr("id", task.subTaskId);
     p.attr("name", "taskNameList");
     p.click(function(e){getTaskId(task.subTaskId)});
     p.text(task.name);
     text.append(p);
     div.append(text);
+    let i = $(document.createElement("i"));
+    i.attr("class" ,"ms-Icon ms-Icon--FavoriteStar iconSize-20");
+    div.append(i);
     $("#subtasklist").append(div); 
 }
 
@@ -180,15 +191,16 @@ function createTask() {
         listInfo.subTask.push(newTask);
         displaySubTasks(newTask);
         tasks.val("");
-        taskName(newTask);
     }
 };
 
 function taskName (taskObject) {
     if(taskObject.status) {
         $("#taskName").attr("class", "task-name");
+        $("#taskNameCheckBox").attr("src" ,"images/circle-unchecked.svg");
     } else {
         $("#taskName").attr("class", "task-name t-n-l");
+        $("#taskNameCheckBox").attr("src" ,"images/circle-checked.svg");
     }
     $("#taskName").val(taskObject.name);
     taskInfo = taskObject;
@@ -213,15 +225,17 @@ function getStepsOfTask(steps) {
  */
 function displaySteps(step) {
     let div = $(document.createElement("div"));
+    let image = $(document.createElement("img"));
     if(step.status) {
         div.attr("class", "steps");
+        image.attr("src", "images/circle.svg");
     } else {
         div.attr("class", "steps-line-through");
+        image.attr("src", "images/step-checked.svg");
     }
     div.attr("id", step.id);
-    let span = $(document.createElement("span"));
-    let image = $(document.createElement("img"));
-    image.attr("src", "images/circle.svg");
+    let span = $(document.createElement("div"));
+    span.attr("class", "p-t-10");
     image.attr("id", step.id);
     image.attr("name", "step-check");
     image.click(function(e){strikeOutStep(step.id)});
@@ -232,12 +246,18 @@ function displaySteps(step) {
     text.val(step.name);
     text.attr("id", step.id);
     div.append(text);
+    let cancel = $(document.createElement("div"));
+    cancel.attr("class", "p-t-14");
     let i = $(document.createElement("i"));
     i.attr("class", "icon fontIcon ms-Icon ms-Icon--Cancel iconSize-16");
-    div.append(i);
+    cancel.append(i);
+    div.append(cancel);
     $("#steps").append(div);
 }
 
+/**
+ * Method used to update the taskName
+ */
 function updateTaskName() {
     let updatedTaskName = $("#taskName").val();
     if(event.keyCode === 13) {
@@ -247,6 +267,10 @@ function updateTaskName() {
     }
 }
 
+/**
+ * Method used to check the status 
+ * @param {object} task 
+ */
 function checkStatus(task) {
     if(!task.status) {
         task.status = true;
@@ -258,18 +282,26 @@ function checkStatus(task) {
     strikeTaskName(task);
 }
 
+/**
+ * To line-through  the taskName by using the 
+ * @param {object} task 
+ */
 function strikeTaskName(task) {
     if($("#taskName").val() == task.name) {
         if(!taskInfo.status) {
-            $("#taskName").attr("class", "task-name t-n-l");
+            $("#taskName").addClass("t-n-l");
+            $("#taskNameCheckBox").attr("src" ,"images/circle-checked.svg");
         }  else {
-            $("#taskName").attr("class", "task-name");
+            $("#taskName").removeClass("t-n-l");
+            $("#taskNameCheckBox").attr("src" ,"images/circle-unchecked.svg");
         }
     } else {
         if(!taskInfo.status) {
-            $("#taskName").attr("class", "task-name t-n-l");
+            $("#taskName").addClass("t-n-l");
+            $("#taskNameCheckBox").attr("src" ,"images/circle-checked.svg");
         }  else {
-            $("#taskName").attr("class", "task-name");
+            $("#taskName").removeClass("t-n-l");
+            $("#taskNameCheckBox").attr("src" ,"images/circle-unchecked.svg");
         }
     }
 }
@@ -295,22 +327,14 @@ function getTaskById(id) {
  * @param {} subId 
  */
 function toggleStep(){
-    
-    if(getElementById("step-aside").className == "finalDiv w-0") {
-        getElementById("step-aside").className = "finalDiv w-360"
-    } else {
-        getElementById("step-aside").className = "finalDiv w-360"
-    }
+    $("#step-aside").addClass("w-360");
+    $("page").addClass("w-651");
 }
 
 function closeToggle() {
-    if(getElementById("step-aside").className == "finalDiv w-360") {
-        getElementById("step-aside").className = "finalDiv w-0"
-    } else {
-        getElementById("step-aside").className = "finalDiv w-0"
-    }
+    $("#step-aside").removeClass("w-360");
+    $("page").removeClass("w-651");
 }
-
 
 /* Method used to create the steps for the particular task  */
 function createStep() {
